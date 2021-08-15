ff89af36c9eb3c0d121ad9f6ebe5e6fab109017e
saishivali <84268745+saishivali@users.noreply.github.com>
2021-08-15 17:58:25 +0530

    Add files via upload

---
+++ PlayerArcher.js
@@ -0,0 +1,43 @@
+class PlayerArcher {
+    constructor(x, y, width, height, archerAngle) {
+      var options = {
+        restitution: 0.8,
+        friction: 1.0,
+        density: 1.0,
+        isStatic: true
+      };
+      this.width = width;
+      this.height = height;
+      this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+      this.image = loadImage("./assets/playerArcher.png");
+      World.add(world, this.body);
+      Matter.Body.setAngle(this.body, -PI / 2);
+      
+    }
+    shoot(){
+      var velocity=p5.vector.fromAngle(playerArcher.angle);
+      velocity.mult(20)
+      Matter.body.setStatic(this.body,false)
+      Matter.body.setVelocity(this.body,{x:velocity.x,y:velocity.y})
+    }
+   display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      if (keyIsDown(DOWN_ARROW) && angle < -1.2) {
+        angle += 0.01;
+        Matter.Body.setAngle(this.body, angle);
+      }
+  
+      if (keyIsDown(UP_ARROW) && angle > -1.9) {
+        angle -= 0.01;
+        Matter.Body.setAngle(this.body, angle);
+      }
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
+  
